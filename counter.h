586d1043958983e6a9a3bfe525b8e824c193310a
vladbusov <to2002to2002@gmail.com>
2019-10-20 16:17:21 +0300

    function

--- counter.h
+++ counter.h
@@ -2,7 +2,22 @@
 // Created by vlad on 20.10.2019.
 //
 
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
 #ifndef POTOKI_COUNTER_H
 #define POTOKI_COUNTER_H
+
+
+#define SIZE_OF_CHUNK 2
+
+int min(int a, int b);
+void charCounter(char* chars, int* cntChars, const char* buffer, int begin, int end);
+void readPipe(int* sharedCnt, int size, int* fd);
+void writePipe(char* countBuff, char* buffer, int* fd, int pid, int i);
+void interProcessCounter(char* buffer, char* countBuff, int* sharedCnt);
+void printResult(int* sharedCnt, int size);
 
 #endif //POTOKI_COUNTER_H
--- main.c
+++ main.c
@@ -1,26 +1,8 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include "counter.h"
 
-
-#define SIZE_OF_CHUNK 2
-
-
-void charCounter(char* chars, int* cntChars, const char* buffer, int begin, int end) {
-    int sizeChars = (int)strlen(chars);
-
-    for (int i = begin; i < end; i++) {
-        for (int j = 0; j < sizeChars; j++) {
-            if (chars[j] == buffer[i])
-                cntChars[j] += 1;
-        }
-    }
-}
-
-int min(int a, int b) {
-    return (a < b) ? a : b;
-}
 
 int main() {
 
@@ -33,57 +15,10 @@ int main() {
     printf("Введите символы для счета:\n");
     scanf("%s", countBuff);
 
-    int num;
-    if (strlen(buffer)%SIZE_OF_CHUNK == 0)
-        num = ((int)strlen(buffer))/SIZE_OF_CHUNK;
-    else
-        num = ((int)strlen(buffer))/SIZE_OF_CHUNK + 1;
-
-
-    int fd[num][2];
-    pid_t pid[num];
-
     int* sharedCnt = calloc(strlen(countBuff),sizeof(int));
 
-
-    for (int i = 0; i < num; i ++) {
-        pipe(fd[i]);
-        pid[i] = fork();
-
-        if (-1 == pid[i]) { /* Error handling ... */ }
-        else if (0 == pid[i]) {
-            close(fd[i][0]);
-
-            int* cnt = calloc(strlen(countBuff),sizeof(int));
-            charCounter(countBuff, cnt, buffer, SIZE_OF_CHUNK*i, min( SIZE_OF_CHUNK*(i+1), (int)strlen(buffer)) );
-
-
-            write(fd[i][1], cnt, sizeof(int)*strlen(countBuff) );
-            free(cnt);
-            exit(0);
-        }
-    }
-
-    for (int i = 0; i < num; i ++) {
-        int buf[strlen(countBuff)];
-        close(fd[i][1]);
-
-        read(fd[i][0], buf, sizeof(buf));
-//        printf("Received %d:\n[", i+1);
-
-        for (int j = 0; j < (int)strlen(countBuff); j++) {
-//            printf("%d, ", buf[i]);
-            sharedCnt[j] += buf[j];
-        }
-//        printf("]\n");
-    }
-
-    printf("Shared counter :\n[");
-
-    for (int i = 0; i < (int)strlen(countBuff); i++) {
-        printf("%d, ", sharedCnt[i]);
-    }
-    printf("]\n");
+    interProcessCounter(buffer, countBuff, sharedCnt);
+    printResult(sharedCnt, (int) strlen(countBuff));
 
     free(sharedCnt);
     return 0;
